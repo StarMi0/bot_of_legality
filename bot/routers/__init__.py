341b5b5902efeb712ce3ef72feac647c6382be97
Ramzan Musaev <starscream2705@gmail.com>
2025-02-01 12:40:10 +0300

    Добавлены новые роутеры

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -5,6 +5,8 @@ from aiogram import Router
 from .users.admin import router as admin_commands_router
 from .users.users import router as users_commands_router
 from .users.lawyers import router as lawyers_commands_router
+from .scripts.chat import router as chat_commands_router
+from .scripts.order_end import router as ending_commands_router
 from .scripts.registration import router as registration_commands_router
 
 router = Router(name=__name__)
@@ -13,6 +15,8 @@ router.include_router(admin_commands_router)
 router.include_router(registration_commands_router)
 router.include_router(users_commands_router)
 router.include_router(lawyers_commands_router)
+router.include_router(ending_commands_router)
+router.include_router(chat_commands_router)
 
 
 
